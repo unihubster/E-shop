47209af9508466cd831443d96ac848a6b1b1a409
Astrovar <ast@var>
2019-11-19 20:31:43 +0200

    fixed issues with adding products to shopping cart and added "load more" function

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -3,6 +3,7 @@
         initBuyBtn();
         $('#addToCart').click(addProductToCart);
         $('#addProductPopup .count').change(calculateCost);
+        $('#loadMore').click(loadMoreProducts);
     };
 
     const showAddProductPopup = function () {
@@ -30,36 +31,52 @@
 
     const addProductToCart = () => {
         const idProduct = $('#addProductPopup').attr('data-id-product');
-        const count = $('#addProductPopup .count').val();
+        const count = parseInt($('#addProductPopup .count').val());
         $('#addToCart').addClass('hidden');
         $('#addToCartIndicator').removeClass('hidden');
+
+        // these 3 lines below are just for this demo
+        const initialCount = parseInt($('#currentShoppingCart #total-count').text());
+        const initialCost = parseFloat($('#currentShoppingCart #total-cost').text());
+        const cost = initialCost + count * parseFloat($('#addProductPopup .price').text().replace(/\D+/, ''));
+
         setTimeout(() => {
             const data = {
-                totalCount: count,
-                totalCost: 2000
+                totalCount: count + initialCount,
+                totalCost: cost.toFixed(2)
             };
             $('#currentShoppingCart .total-count').text(data.totalCount);
             $('#currentShoppingCart .total-cost').text(data.totalCost);
-            $('#currentShoppingCart').removeClass('hidden');
             $('#addProductPopup').modal('hide');
+            $('#currentShoppingCart').removeClass('hidden');
         }, 800);
     };
 
     const calculateCost = () => {
         const priceStr = $('#addProductPopup .price').text();
-        const price = parseFloat(priceStr.replace('$', ' ')) // just to keep simple this example
+        const moneySymbol = priceStr.match(/\D+/);
+        const price = parseFloat(priceStr.replace(moneySymbol, ''))
         const count = parseInt($('#addProductPopup .count').val());
         const min = parseInt($('#addProductPopup .count').attr('min'));
         const max = parseInt($('#addProductPopup .count').attr('max'));
         if(count >= min && count <= max) {
             const cost = price * count;
-            $('#addProductPopup .cost').text('$ ' + cost.toFixed(2));
+            $('#addProductPopup .cost').text(moneySymbol + cost.toFixed(2));
         } else {
             $('#addProductPopup .count').val(1);
             $('#addProductPopup .cost').text(priceStr);
         }
     };
     
+    const loadMoreProducts = () => {
+        $('#loadMore').addClass('hidden');
+        $('#loadMoreIndicator').removeClass('hidden');
+        setTimeout(() => {
+            $('#loadMoreIndicator').addClass('hidden');
+            $('#loadMore').removeClass('hidden');
+        }, 800);
+    };
+
     init();
 
 });
