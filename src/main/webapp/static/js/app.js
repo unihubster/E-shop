79c82f0d77d0777978cfa7695e4b9a14bb2373de
Astrovar <ast@var>
2019-11-26 18:47:13 +0200

    checkboxes "all" when all filters selected

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -4,6 +4,7 @@
         $('#addToCart').click(addProductToCart);
         $('#addProductPopup .count').change(calculateCost);
         $('#loadMore').click(loadMoreProducts);
+        initSearchForm();
     };
 
     const showAddProductPopup = function () {
@@ -59,7 +60,7 @@
         const count = parseInt($('#addProductPopup .count').val());
         const min = parseInt($('#addProductPopup .count').attr('min'));
         const max = parseInt($('#addProductPopup .count').attr('max'));
-        if(count >= min && count <= max) {
+        if (count >= min && count <= max) {
             const cost = price * count;
             $('#addProductPopup .cost').text(moneySymbol + cost.toFixed(2));
         } else {
@@ -67,7 +68,7 @@
             $('#addProductPopup .cost').text(priceStr);
         }
     };
-    
+
     const loadMoreProducts = () => {
         $('#loadMore').addClass('hidden');
         $('#loadMoreIndicator').removeClass('hidden');
@@ -76,6 +77,44 @@
             $('#loadMore').removeClass('hidden');
         }, 800);
     };
+
+    const initSearchForm = () => {
+        /*  // original code whithout select all when all checkboxes are selected
+                $('#allCategories').click(function () {
+                    $('.categories .search-option').prop('checked', $(this).is(':checked'));
+                });
+                $('.categories .search-option').click(function () {
+                    $('#allCategories').prop('checked', false);
+                });
+                $('#allProducers').click(function () {
+                    $('.producers .search-option').prop('checked', $(this).is(':checked'));
+                });
+                $('.producers .search-option').click(function () {
+                    $('#allProducers').prop('checked', false);
+                });
+         */
+        checkboxer('#allProducers', '.producers .search-option');
+        checkboxer('#allCategories', '.categories .search-option');
+    };
+
+    function checkboxer(select_all, select_one) {
+        // https://www.sanwebe.com/2014/01/how-to-select-all-deselect-checkboxes-jquery
+        //select all checkboxes
+        $(select_all).change(function () {  //"select all" change 
+            $(select_one).prop('checked', $(this).prop('checked')); //change all ".checkbox" checked status
+        });
+        //".checkbox" change 
+        $(select_one).change(function () {
+            //uncheck "select all", if one of the listed checkbox item is unchecked
+            if (false == $(this).prop('checked')) { //if this item is unchecked
+                $(select_all).prop('checked', false); //change "select all" checked status to false
+            }
+            //check "select all" if all checkbox items are checked
+            if ($(select_one + ':checked').length == $(select_one).length) {
+                $(select_all).prop('checked', true);
+            }
+        });
+    }
 
     init();
 
